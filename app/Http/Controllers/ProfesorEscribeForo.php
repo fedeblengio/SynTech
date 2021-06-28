81afe025495595abacf9bacaab18115c10e2c9a2
root <root@localhost.localdomain>
2021-06-27 22:12:34 -0400

    Subir archivos foro

--- app/Http/Controllers/ProfesorEscribeForo.php
+++ app/Http/Controllers/ProfesorEscribeForo.php
@@ -21,31 +21,61 @@ class ProfesorEscribeForo extends Controller
 
     public function store(Request $request)
     {
-        try {
+        /* try { */
                 $datosForo = new datosForo;
                 $datosForo->idForo = $request->idForo;
                 $datosForo->idUsuario = $request->idUsuario;
+                $datosForo->titulo = $request->titulo;
                 $datosForo->mensaje = $request->mensaje;
-                $datosForo->datos = "3";
-                $datosForo->save();
-                return response()->json(['status' => 'Success'], 200);
-        } catch (\Throwable $th) {
-                return response()->json(['status' => 'Bad Request'], 400);
-             }   
+                
+
+                if($request->hasFile("archivo")){
+                    $file=$request->archivo;
+                    
+                    $nombre = "pdf_".time().".".$file->guessExtension();
+        
+                    $ruta = public_path("pdf/".$nombre);
+                    $ruta2 = public_path("docx/".$nombre);
+                     /* $request->archivo->store('public'); */
+        
+                    if($file->guessExtension()=="pdf"){
+                        copy($file, $ruta);
+                        $datosForo->datos = $ruta;
+                        $datosForo->save();
+                        return response()->json(['status' => 'Success'], 200);
+                        if($file->guessExtension()=="docx"){
+                            copy($file, $ruta2);
+                            $datosForo->datos = $ruta2;
+                            $datosForo->save();
+                        return response()->json(['status' => 'Success'], 200);
+                        }
+                    }else{
+                        return response()->json(['status' => 'Error'], 406);
+                    } 
+                //* * } */
+
+                /* $datosForo->datos = $request->archivo; */
+                
+               /*  return response()->json(['status' => 'Success'], 200); */
+      /*   } catch (\Throwable $th) {
+                return response()->json($th);
+             }    */
 
     }
+}
 
     public function update(Request $request)
     {
         $modificarDatosForo = datosForo::where('id', $request->idDatos)->first();
        
-        //try{
+        try{
+            $modificarDatosForo->titulo = $request->titulo;
             $modificarDatosForo->mensaje = $request->mensaje;
             $modificarDatosForo->save();
             return response()->json(['status' => 'Success'], 200);
-        //} catch (\Throwable $th) {
+        } catch (\Throwable $th) {
             return response()->json(['status' => 'Bad Request'], 400);
-        //}
+        }
     }
 
     public function destroy(Request $request)
