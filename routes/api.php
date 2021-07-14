13852a06f385a8cbe0d0af3cfaf2e15c7d0d9d66
root <root@localhost.localdomain>
2021-07-13 21:48:42 -0400

    agregar middleware

--- routes/api.php
+++ routes/api.php
@@ -27,39 +27,39 @@ Route::get('/test', function (){
 
 
 //usuarios
-Route::put('/usuario','App\Http\Controllers\usuariosController@update');
+Route::put('/usuario','App\Http\Controllers\usuariosController@update')->middleware('verificar_token');
 
 //foros
-Route::get('/foros','App\Http\Controllers\ProfesorEscribeForo@index');
-Route::get('/foro','App\Http\Controllers\ProfesorEscribeForo@show');
-Route::get('/archivo','App\Http\Controllers\ProfesorEscribeForo@traerArchivo');
-Route::post('/foro','App\Http\Controllers\ProfesorEscribeForo@store');
-Route::put('/foro','App\Http\Controllers\ProfesorEscribeForo@update');
-Route::delete('/foro','App\Http\Controllers\ProfesorEscribeForo@destroy');
+Route::get('/foros','App\Http\Controllers\ProfesorEscribeForo@index')->middleware('verificar_token');
+Route::get('/foro','App\Http\Controllers\ProfesorEscribeForo@show')->middleware('verificar_token');
+Route::get('/archivo','App\Http\Controllers\ProfesorEscribeForo@traerArchivo')->middleware('verificar_token');
+Route::post('/foro','App\Http\Controllers\ProfesorEscribeForo@store')->middleware('verificar_token');
+Route::put('/foro','App\Http\Controllers\ProfesorEscribeForo@update')->middleware('verificar_token');
+Route::delete('/foro','App\Http\Controllers\ProfesorEscribeForo@destroy')->middleware('verificar_token');
 
 
-Route::get('/profesor-grupo','App\Http\Controllers\ProfesorGrupo@listarProfesorGrupo');
+Route::get('/profesor-grupo','App\Http\Controllers\ProfesorGrupo@listarProfesorGrupo')->middleware('verificar_token');
 
 
-Route::get('/alumno','App\Http\Controllers\alumnos@show');
+Route::get('/alumno','App\Http\Controllers\alumnos@show')->middleware('verificar_token');
 
-Route::get('/profesor-foro','App\Http\Controllers\ProfesorEscribeForo@index');
+Route::get('/profesor-foro','App\Http\Controllers\ProfesorEscribeForo@index')->middleware('verificar_token');
 
-Route::get('/foro-grupo','App\Http\Controllers\ProfesorGrupo@listarDatosForo');
+Route::get('/foro-grupo','App\Http\Controllers\ProfesorGrupo@listarDatosForo')->middleware('verificar_token');
 
 
 
 //tareas
-Route::get('/traerTareasGrupo','App\Http\Controllers\ProfesorCreaTarea@traerTareasGrupo');
-Route::get('/traerArchivo','App\Http\Controllers\ProfesorCreaTarea@traerArchivo');
-Route::post('/tarea','App\Http\Controllers\ProfesorCreaTarea@store');
-Route::get('/tareas','App\Http\Controllers\ProfesorCreaTarea@show');
+Route::get('/traerTareasGrupo','App\Http\Controllers\ProfesorCreaTarea@traerTareasGrupo')->middleware('verificar_token');
+Route::get('/traerArchivo','App\Http\Controllers\ProfesorCreaTarea@traerArchivo')->middleware('verificar_token');
+Route::post('/tarea','App\Http\Controllers\ProfesorCreaTarea@store')->middleware('verificar_token');
+Route::get('/tareas','App\Http\Controllers\ProfesorCreaTarea@show')->middleware('verificar_token');
 
 //alumnos
-Route::get('/alunos','App\Http\Controllers\AlumnoEntregaTarea@index');
-Route::get('/alumno','App\Http\Controllers\AlumnoEntregaTarea@traerTareasMateria');
-Route::post('/alumno','App\Http\Controllers\AlumnoEntregaTarea@store');
-Route::get('/archivoAlumno','App\Http\Controllers\AlumnoEntregaTarea@traerArchivo');
-Route::put('/alumno','App\Http\Controllers\AlumnoEntregaTarea@update');
+Route::get('/alumnosTarea','App\Http\Controllers\AlumnoEntregaTarea@index')->middleware('verificar_token');
+Route::get('/alumnoTarea','App\Http\Controllers\AlumnoEntregaTarea@traerTareasMateria')->middleware('verificar_token');
+Route::post('/alumnoTarea','App\Http\Controllers\AlumnoEntregaTarea@store')->middleware('verificar_token');
+Route::get('/archivoAlumnoTarea','App\Http\Controllers\AlumnoEntregaTarea@traerArchivo')->middleware('verificar_token');
+Route::put('/alumnoTarea','App\Http\Controllers\AlumnoEntregaTarea@update')->middleware('verificar_token');
 
 
