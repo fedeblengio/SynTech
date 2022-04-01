8168a61afd04a774820adfebd3997679ba971e54
root <root@localhost.localdomain>
2022-04-01 19:54:01 -0300

    lim. request  120

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,8 +40,8 @@ class Kernel extends HttpKernel
         ],
 
          'api' => [
-           /*  'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class, */
+            'throttle:120,1',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ], 
     ];
 
