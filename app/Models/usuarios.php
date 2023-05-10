3ea53ebc072094e6fdd30c5d21d3087197c32e4b
Agustin Kuster <akusterpiriz@gmail.com>
2023-05-09 22:00:41 -0300

    Integrate notification for alumno y remove raw q

--- app/Models/usuarios.php
+++ app/Models/usuarios.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Notifications\Notifiable;
 class usuarios extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Notifiable;
 
     protected $fillable = [
         'nombre',
@@ -18,4 +19,15 @@ class usuarios extends Model
     ];
 
     protected $guarded = ['id','ou'];
+
+    
+    public function getNotifications($all = false)
+    {
+        if ($all) {
+            return $this->notifications;
+        }
+        return $this->unreadNotifications->where('deleted_at', null);
+    }
+
+
 }
